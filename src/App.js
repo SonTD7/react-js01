8e84cca0b24b962906019838f56093bd99698ac4
TRANDUYSON <sontddeveloper@gmail.com>
2022-09-12 21:25:09 +0700

    summit1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Ristorante from "./components/Ristorante";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <nav class="navbar navbar-light bg-warning">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                Ristorante Con Fusion
+              </a>
+            </div>
+          </nav>
+        </div>
+        <Ristorante/>
+      </section>
+    </main>
   );
 }
 
---
+++ src/components/Ristorante.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+
+function Ristorante() {
+    return (
+        <div className="row">
+           <div className="col-md-6">
+            <img src="https://banhthuanphong.com/Upload/images/Tin%20T%E1%BB%A9c/hinh-anh-banh-trung-thu-dep(1).jpg"></img>
+           </div>
+        </div>
+    )
+};
+
+export default Ristorante;
